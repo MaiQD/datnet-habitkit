aac2bb89cd131b74418a7227b89168d4d59cd416
datmai <q.dat2710@gmail.com>
2025-10-09 02:02:44 +0700

    Add new migration to change DailyReminderTime type from TimeOnly to string in UserSettings
    - Created a new migration to alter the DailyReminderTime column in the UserSettings table, changing its type from TimeOnly to string and making it non-nullable.
    - Updated the ApplicationDbContextModelSnapshot to reflect this change.

---
+++ HabitKitClone/Data/Migrations/20251008185659_new migration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HabitKitClone.Migrations
+{
+    /// <inheritdoc />
+    public partial class newmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DailyReminderTime",
+                table: "UserSettings",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(TimeOnly),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeOnly>(
+                name: "DailyReminderTime",
+                table: "UserSettings",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
